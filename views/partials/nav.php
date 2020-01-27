8ecb062b46cb0837fa0da40176d1e56fdd4b147d
abdulloooh <abdullahakinwumi@gmail.com>
2020-01-27 05:05:49 +0100

    cleared files to start afresh on lesson 16,routers..backup at lesson 15 branch

--- views/partials/nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li><a href="controller\home.php">Home</a></li>
-    <li><a href="controller\about.php">About us</a></li>
-    <li><a href="controller\contact.php">Contact Us</a></li>
-</ul>
