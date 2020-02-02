4e057d6244c93025ca251827e15989ba53f88450
abdulloooh <abdullahakinwumi@gmail.com>
2020-02-02 18:04:07 +0100

    init commit in report branch

--- views/about.view.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "views/partials/header.php";
-require "views/partials/nav.php";
-?>
-<h1>About Us</h1>
-
-<?php require "views/partials/footer.php";
--- views/contact.view.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "views/partials/header.php";
-require "views/partials/nav.php";
-?>
-<h1>Contact Us</h1>
-
-<?php require "views/partials/footer.php";
